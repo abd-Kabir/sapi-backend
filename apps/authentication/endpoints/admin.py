2b48794a62967edcec56b8ed8c9fe3b9066b3c80
Umarov Kabir <kabirumarov@gmail.com>
2025-06-05 11:47:55 +0500

    admin: urls;

---
+++ apps/authentication/endpoints/admin.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from apps.authentication.routes.admin import (AdminCreatorListAPIView, AdminCreatorSAPIShareAPIView,
+                                              AdminCreatorRetrieveAPIView)
+
+urlpatterns = [
+    path('admin/creators/', AdminCreatorListAPIView.as_view(), name='admin_creators'),
+    path('admin/creator/<int:pk>/', AdminCreatorRetrieveAPIView.as_view(), name='admin_creator'),
+    path('admin/creator/<int:pk>/sapi-share', AdminCreatorSAPIShareAPIView.as_view(), name='admin_creators_sapi_share'),
+]
