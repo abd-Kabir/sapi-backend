de412243602bfc74f2e34586b73db3d30774ee3a
abd-Kabir <kabirumarov@gmail.com>
2025-06-27 23:22:41 +0500

    log_post publication time

--- apps/content/managers.py
+++ apps/content/managers.py
@@ -1,10 +1,15 @@
+import logging
+
 from django.db.models import Q, Manager
 from django.utils.timezone import now
 
+logger = logging.getLogger()
 
 class PostManager(Manager):
     def get_queryset(self):
-        return super().get_queryset().filter(
+        queryset = super().get_queryset()
+        [logger.debug(f'Publication_time of id:{i.id}-- {i.publication_time};') for i in queryset]
+        return queryset.filter(
             (Q(publication_time__lte=now()) | Q(publication_time=None)),
             is_posted=True, is_deleted=False, is_blocked=False, user__is_blocked_by__isnull=True
         )
