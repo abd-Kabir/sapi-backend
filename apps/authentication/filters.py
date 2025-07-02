41c327cef6743dbb4c250a59bcd5ed77d699aa46
Umarov Kabir <kabirumarov@gmail.com>
2025-07-02 10:27:44 +0500

    filters;

--- apps/authentication/filters.py
+++ apps/authentication/filters.py
@@ -1,7 +1,45 @@
 import django_filters
+from django.db.models import Q
 
-from apps.authentication.models import NotificationDistribution
+from apps.authentication.models import NotificationDistribution, User
 from apps.content.models import Report
+
+
+class AdminCreatorFilter(django_filters.FilterSet):
+    search = django_filters.CharFilter(method='filter_search')
+    date_from = django_filters.DateFilter(field_name='date_joined', lookup_expr='date__gte')
+    date_to = django_filters.DateFilter(field_name='date_joined', lookup_expr='date__lte')
+    category = django_filters.NumberFilter(field_name='category_id')
+    user_type = django_filters.NumberFilter(method='filter_user_type')
+    status = django_filters.NumberFilter(method='filter_status')
+
+    class Meta:
+        model = User
+        fields = ['category_id']
+
+    def filter_search(self, queryset, name, value):
+        return queryset.filter(
+            Q(username__icontains=value) |
+            Q(temp_username__icontains=value) |
+            Q(phone_number__icontains=value) |
+            Q(temp_phone_number__icontains=value)
+        )
+
+    def filter_user_type(self, queryset, name, value):
+        if value == 1:
+            return queryset.filter(is_creator=True)
+        elif value == 0:
+            return queryset.filter(is_creator=False)
+        return queryset
+
+    def filter_status(self, queryset, name, value):
+        if value == 0:
+            return queryset.filter(is_creator=False)
+        elif value == 1:
+            return queryset.filter(is_creator=True, is_blocked_by__isnull=True)
+        elif value == 2:
+            return queryset.filter(is_blocked_by__isnull=False)
+        return queryset
 
 
 class ReportFilter(django_filters.FilterSet):
