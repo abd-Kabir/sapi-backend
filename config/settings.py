ffde70a7a325b6ec5b3571fff5778292a3d53e11
abd-Kabir <kabirumarov@gmail.com>
2025-07-16 00:07:55 +0500

    Refactor serializers and adjust JWT settings.
    Updated `AnswerOptionSerializer` to include additional context in its representation for better analytics. Modified JWT token lifetimes to align with new authentication policies. Added request context to `ChatSettings` serializer and updated `docker-compose.yml` to include log file mounting.

--- config/settings.py
+++ config/settings.py
@@ -309,8 +309,8 @@ LOGGING = {
 
 # SIMPLE-JWT
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=7),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=365),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': False,
     'UPDATE_LAST_LOGIN': False,
